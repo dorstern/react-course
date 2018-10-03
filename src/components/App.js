a7dbce77146aed0184c437ac002ae0e42ac03d36
Dor Stern <dor@bidalgo.com>
2018-10-03 17:09:34 +0300

    Route example

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,34 @@
-import React from 'react';
-import Button from "./Button";
-import Input from './Input';
-import { Page } from "./Page";
+import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const About = (props) => (
+    <div>About
+        <h3 onClick={ () => props.history.push('/index') }>Go to index</h3>
+    </div>
+);
+const Home = () => (<div>Home</div>);
+const Index = () => (<div>Index</div>);
+const Notfound = () => (<div>404 Page not found</div>);
+const User = (props) => (
+    <div>
+        <span>User:</span>
+        <span>{props.match.params.id}</span>
+    </div>
+);
 
 export default class App extends React.Component {
-    state = {
-        counter: 0
-    };
-
-    //increase = () => {} - is bind function and that's why we can use this
-    //increase() {} - we can not use this inside because it's another scope
-    increase = () => {
-        this.setState({counter: this.state.counter = this.state.counter + 1});
-    };
-
-    decrease = () => {
-        this.setState({counter: this.state.counter = this.state.counter - 1});
-    };
-
     render() {
         return (
-            <div>
-                <Input label={ "Insert input here"}/>
-                <Button onClick={ this.increase } label={"increase"}/>
-                <Button onClick={ this.decrease } label={"decrease"}/>
-            </div>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/about" component={About}/>
+                    <Route exact path="/home" component={Home}/>
+                    <Route exact path="/index" component={Index}/>
+                    <Route exact path="/users/:id?" component={User}/>
+                    <Route path="*" component={Notfound}/>
+                </Switch>
+            </Router>
         )
     }
 }
